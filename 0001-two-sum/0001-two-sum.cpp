b8c17fa41fd624e9689125e45f0ee3c9cbae1d8a
maimoona-mustafvi <147592921+maimoona-mustafvi@users.noreply.github.com>
2024-10-19 21:04:34 +0500

    Time: 0 ms (100.00%), Space: 14.4 MB (36.50%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i=0 ; i<nums.size(); i++){
-            for(int j=i+1 ; j<nums.size(); j++){
-                if(nums[i]+nums[j] == target){
-                    return {i,j};
-                }
-                else{
-                    continue;
-                }
-            }
+       // Build and check hash table while iterating
+    unordered_map<int, int> numMap;  // <value, index>
+    
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        
+        // If complement exists in the hash table and isn't the same element
+        if (numMap.find(complement) != numMap.end()) {
+            return {numMap[complement], i};
         }
-        return {};
+        
+        // Insert the current number and its index into the hash table
+        numMap[nums[i]] = i;
+    }
+    
+    // Return an empty vector if no solution is found
+    return {};
     }
 };
